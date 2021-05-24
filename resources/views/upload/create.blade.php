57e4d332228a45bae114cc349ec25a126a378dbd
Paulo Henrique <phvcandido@gmail.com>
2021-05-24 19:53:31 -0300

    Atualizando listagem das imagens e configurações do upload em JS

--- resources/views/upload/create.blade.php
+++ resources/views/upload/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,44 +13,40 @@
         .container {
             max-width: 500px;
         }
-        dl, ol, ul {
+
+        dl,
+        ol,
+        ul {
             margin: 0;
             padding: 0;
             list-style: none;
         }
+
         .imgPreview img {
             padding: 8px;
             max-width: 100px;
         }
     </style>
 </head>
+
 <body>
-
-
-<!-- <form action="{{ route('store') }}" method="POST" enctype="multipart/form-data">
-@csrf
-    <label for="img_name"> Nome da imagem </label>
-    <input type="file" name="img_path[]" class="image" multiple="multiple">
-    <button class="sub"> Enviar </button>
-</form> -->
-
-<div class="container mt-5">
-        <form class="w-100 d-flex flex-column align-items-center" action="{{route('store')}}" method="post" enctype="multipart/form-data">
+    <div class="container mt-5">
+        <form id="imageForm" class="w-100 d-flex flex-column align-items-center" action="{{route('store')}}" method="post" enctype="multipart/form-data">
             @csrf
             @if ($message = Session::get('success'))
-                <div class="alert alert-success">
-                    <strong>{{ $message }}</strong>
-                </div>
+            <div class="alert alert-success">
+                <strong>{{ $message }}</strong>
+            </div>
             @endif
 
             @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
             @endif
 
             <div class="user-image mb-3 text-center">
@@ -59,75 +56,76 @@
                 <div class="" id="upload-demo"> </div>
             </div>
 
-            <!-- <fielset class="form-group">
-                <label class="" for="img_name"> Nome da(s) imagem(s)
-                <input class="form-control" type="text" name="img_name" id="img_name" placeholder="nome da imagem">
-            </fieldset> -->
             <div class="custom-file mt-3">
-                <input type="file" name="img_path_temporario[]" class="custom-file-input" id="images" multiple>
-                <input style="display:none;" type="file" name="img_path[]" class="custom-file-input" id="images-hidden" multiple>
+                <input type="file" name="img_path_temporario" class="custom-file-input" id="images">
+                <input type="hidden" name="img_path" id="images-hidden">
                 <label class="custom-file-label" for="images">Escolha a(s) imagem(s) </label>
             </div>
 
-            <button type="submit" name="submit" class="btn btn-primary btn-block mt-4">
+            <button type="submit" class="btn btn-primary btn-block mt-4">
                 Upload Images
             </button>
+
         </form>
     </div>
-<script src="{{ asset('Croppie/croppie.js') }}"> </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script>
-        $(function() {
-        // Multiple images preview with JavaScript
-        let containImages = [];
-        var multiImgPreview = function(input, imgPreviewPlaceholder) {
-            // $(imgPreviewPlaceholder).html('');
-
-            if (input.files) {
-                var filesAmount = input.files.length;
-
-
-                for (i = 0; i < filesAmount; i++) {
-
-
-                    var reader = new FileReader();
-
-                    reader.onload = function(event) {
-                        $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(imgPreviewPlaceholder);
-                    }
-
-                    reader.readAsDataURL(input.files[i]);
-                    containImages.push(input.files[i]);
-                    console.log(input.files[i]);
+    <script src="{{ asset('Croppie/croppie.js') }}"> </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script>
+        // $(function() {
+        function inicializarCroppie() {
+            const el = document.querySelector('#upload-demo');
+            const options = {
+                enableExif: true,
+                viewport: {
+                    width: 200,
+                    height: 200,
+                    type: 'circle'
+                },
+                boundary: {
+                    width: 300,
+                    height: 300
                 }
 
-                // $('#images-hidden').val(containImages);
-                const fl = new FileList();
-                fl = containImages;
-                document.querySelector('#images-hidden').files = fl;
-                console.log(containImages);
-            }
-
-        };
-
-            $('#images').on('change', function() {
-                multiImgPreview(this, '#upload-demo');
-            });
-
-        uploadCrop = document.querySelector('#upload-demo') = new Croppie({
-        enableExif: true,
-        viewport: {
-            width: 200,
-            height: 200,
-            type: 'circle'
-        },
-        boundary: {
-            width: 300,
-            height: 300
+            };
+            return new Croppie(el, options);
         }
 
+        const croppedDiv = inicializarCroppie();
+
+        var multiImgPreview = function(input) {
+            const that = this;
+            var reader = new FileReader();
+
+            reader.onload = function(event) {
+                const url = event.target.result;
+                croppedDiv.bind({
+                    url
+                });
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+
+        document.querySelector('#imageForm').addEventListener('submit', async function(e) {
+            e.preventDefault();
+            const imageFormHidden = document.querySelector('#imageForm');
+            const imageHidden = document.querySelector('#images-hidden');
+            croppedDiv.result('base64').then((base64) => {
+                imageHidden.value = base64;
+                console.log(imageFormHidden)
+
+            }).then(() => {
+                imageFormHidden.submit();
+            });
+
         });
-});
+
+        document.querySelector('#images').addEventListener('change', function() {
+            multiImgPreview(this);
+        });
+
+        // });
     </script>
 </body>
+
 </html>
--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -1,11 +1,11 @@
 <h1> Listagem das imagens </h1>
 
 @if (!empty($uploads))
-    @foreach ($uploads as $upload)
+@foreach ($uploads as $upload)
 
-    <p> {{ $upload->img_name }} </p>
-    <img src="img/uploads/{{ $upload->img_path }}" alt="{{ $upload->img_name }}">
-    @endforeach
+<p> {{ $upload->img_name }} </p>
+<img src="{{ $upload->img_path }}" alt="{{ $upload->img_name }}">
+@endforeach
 @endif
 
 <a href="/create">
