8f5bd804e75df0cd11be3bde16f396e215baef71
Paulo Henrique <phvcandido@gmail.com>
2021-05-24 19:52:59 -0300

    Atualizando configuração da criação da tabela para longblob

--- database/migrations/2021_05_09_181420_create_uploads_table.php
+++ database/migrations/2021_05_09_181420_create_uploads_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUploadsTable extends Migration
 {
@@ -16,9 +17,10 @@ class CreateUploadsTable extends Migration
         Schema::create('uploads', function (Blueprint $table) {
             $table->id();
             $table->string('img_name');
-            $table->string('img_path');
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE uploads ADD img_path  LONGBLOB");
     }
 
     /**
